beb981fceab70f0d23f8cf4f9453b130f5e490d6
AlexSomovilla <alexsomovilla@gmail.com>
2019-07-26 17:07:15 +0200

    Add files via upload

---
+++ natural_language_processing_alex_DecissionTree.r
@@ -0,0 +1,62 @@
+# Natural Language Processing
+
+# Importar el dataset
+dataset_original = read.delim('Restaurant_Reviews.tsv', quote = "",
+                      stringsAsFactors = FALSE)
+
+# Limpieza de textos
+# install.packages("tm")
+# install.packages("SnowballC")
+library(tm)
+library(SnowballC)
+corpus = VCorpus(VectorSource(dataset_original$Review))
+corpus = tm_map(corpus, content_transformer(tolower))
+# Consultar el primer elemento del corpus
+# as.character(corpus[[1]])
+corpus = tm_map(corpus, removeNumbers)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords(kind = "en"))
+corpus = tm_map(corpus, stemDocument)
+corpus = tm_map(corpus, stripWhitespace)
+
+# Crear el modelo Bag of Words
+dtm = DocumentTermMatrix(corpus)
+dtm = removeSparseTerms(dtm, 0.999)
+
+dataset = as.data.frame(as.matrix(dtm))
+dataset$Liked = dataset_original$Liked
+
+# Codificar la variable de clasificación como factor
+dataset$Liked = factor(dataset$Liked, levels = c(0,1))
+
+# Dividir los datos en conjunto de entrenamiento y conjunto de test
+# install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Liked, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+testing_set = subset(dataset, split == FALSE)
+
+# Ajustar el SVM con el conjunto de entrenamiento.
+# install.packages("e1071")
+install.packages("rpart")
+library(rpart)
+classifier = rpart(formula = Liked ~ .,
+                   data = training_set)
+
+# Predicción de los resultados con el conjunto de testing
+y_pred = predict(classifier, newdata = testing_set[,-692],
+                 type = "class")
+
+# Crear la matriz de confusión
+cm = table(testing_set[, 692], y_pred)
+
+TN = cm[1, 1]
+FN = cm[2, 1]
+TP = cm[2, 2]
+FP = cm[1, 2]
+accuracy = (TP + TN) / (TP + TN + FP + FN)
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+specificity = TN / (TN + FP)
+F1_score = 2 * precision * recall / (precision + recall)
